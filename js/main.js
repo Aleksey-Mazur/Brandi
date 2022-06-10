7f03bd839c4bbdeca4b43ee32c511838bbacb2d3
Aleksey <1aleksey.mazur@gmail.com>
2022-06-10 20:34:37 +0300

    _header.scss, _gallery.scss have been added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const myCarouselElement = document.querySelector("#carouselExampleCaptions");
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 100000,
+  touch: true,
+  pause: false,
+  ride: "carousel",
+});
